cc66267df501d7cc0ed1d5939e9593e957a93abf
vikinglee1982 <750820181@qq.com>
2023-01-30 16:29:36 +0800

    修复储存图片路径错误的bug；添加数组式批量添加方法

--- MeaPHP/Core/Tools/Save.php
+++ MeaPHP/Core/Tools/Save.php
@@ -23,6 +23,8 @@ class Save
         // 'msg' => null,
         //错误：返回错误原因
     );
+    //用户多图片数组储存时临时储存返回结果用
+    private $tempRes = array();
 
 
 
@@ -45,6 +47,66 @@ class Save
             self::$obj = new self();
         }
         return self::$obj;
+    }
+    //储存多图
+    public function batchImage($arr)
+    {
+        //通过递归方式调用
+        //先验证参数是否正确
+        if (!is_array($arr)) {
+            $this->res['status'] = 'error';
+            $this->res['msg'] = '入参必须是数组';
+        } else {
+            //检查数组的数据结构
+            $fault = false;
+            foreach ($arr as $k => $v) {
+                if (!$v['resource'] || !$v['path']) {
+                    $fault = true;
+                    break;
+                }
+            }
+            //结构式错误的
+            if ($fault) {
+                $this->res['status'] = 'error';
+                $this->res['msg'] = '入参的数组结构错误;
+                [
+                    0:{
+                        resource:图片资源,
+                        path:从项目根路径下开始的储存路径,
+                    }
+                    1:{
+                        resource:图片资源,
+                        path:从项目根路径下开始的储存路径,
+                    }
+                ]';
+            } else {
+
+                $this->recursion($arr);
+
+                // $this->res['status'] = 'ok';
+                // $this->res['msg'] = $arr;
+            }
+            return $this->res;
+        }
+    }
+    //递归循环调用储存
+    private function recursion($arr, $i = 0)
+    {
+        $res = $this->image($arr[$i]['resource'], $arr[$i]['path']);
+        if ($res['status'] == 'ok') {
+            //临时储存已经储存的图片路径
+            array_push($this->tempRes, $this->res['data']);
+            $i++;
+            if ($arr[$i]) {
+                $this->recursion($arr, $i);
+            } else {
+                $this->res['status'] = 'ok';
+                $this->res['data'] = $this->tempRes;
+                return $this->$res;
+            }
+        } else {
+            return $this->res;
+        }
     }
     //生成毫秒级时间戳
     public function image($file = null, $folderName = null, $fileName = null)
@@ -81,6 +143,9 @@ class Save
 
             //合成需要存放文件的路径
             // $userDir = iconv('utf-8', 'gbk', $this->basicsPath . '/' . $folderName . '/' . $fileType);
+            if (substr($folderName, 0, 1) !== '/') {
+                $folderName = '/' . $folderName;
+            }
 
             $userDir = iconv('utf-8', 'gbk', $_SERVER['DOCUMENT_ROOT']  . $folderName);
 
@@ -97,6 +162,7 @@ class Save
             if (file_exists($src)) {
                 $this->res['status'] = "error";
                 $this->res['msg'] = '文件名重复';
+                // $this->res['msg'] = $src;
                 // return "error:已经有这个文件了";
             } else {
                 if (move_uploaded_file($file['tmp_name'], $src)) {
