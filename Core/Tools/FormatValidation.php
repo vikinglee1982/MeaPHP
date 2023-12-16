a2ad273ba423500eb72d07439ebdaedd86dfb754
vikinglee1982 <750820181@qq.com>
2023-12-16 16:45:34 +0800

    修复验证身份证号码工具类，长度超出18位判断失误的错误

--- Core/Tools/FormatValidation.php
+++ Core/Tools/FormatValidation.php
@@ -87,34 +87,37 @@ class FormatValidation
             // return false;
             $this->res['status'] = 'error';
             $this->res['msg'] = '身份证号码只能是18位';
+        } else {
+            $idcard_base = substr($idcard, 0, 17);
+            // 取出校验码
+            $verify_code = substr($idcard, 17, 1);
+            // 加权因子
+            $factor = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
+            // 校验码对应值
+            $verify_code_list = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
+            // 根据前17位计算校验码
+            $total = 0;
+            for ($i = 0; $i < 17; $i++) {
+                $total += substr($idcard_base, $i, 1) * $factor[$i];
+            }
+            // 取模
+            $mod = $total % 11;
+            // 比较校验码
+            if ($verify_code == $verify_code_list[$mod]) {
+                // return $idcard;
+                $this->res['status'] = 'ok';
+                $this->res['data'] = $idcard;
+                $this->res['msg'] = '正确的身份证号码';
+            } else {
+                $this->res['status'] = 'error';
+                $this->res['msg'] = '错误的身份证号码验证码';
+                $this->res['data'] = $idcard;
+                // return false;
+            }
         }
         // 取出本体码
-        $idcard_base = substr($idcard, 0, 17);
-        // 取出校验码
-        $verify_code = substr($idcard, 17, 1);
-        // 加权因子
-        $factor = array(7, 9, 10, 5, 8, 4, 2, 1, 6, 3, 7, 9, 10, 5, 8, 4, 2);
-        // 校验码对应值
-        $verify_code_list = array('1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2');
-        // 根据前17位计算校验码
-        $total = 0;
-        for ($i = 0; $i < 17; $i++) {
-            $total += substr($idcard_base, $i, 1) * $factor[$i];
-        }
-        // 取模
-        $mod = $total % 11;
-        // 比较校验码
-        if ($verify_code == $verify_code_list[$mod]) {
-            // return $idcard;
-            $this->res['status'] = 'ok';
-            $this->res['data'] = $idcard;
-            $this->res['msg'] = '正确的身份证号码';
-        } else {
-            $this->res['status'] = 'error';
-            $this->res['msg'] = '错误的身份证号码验证码';
-            $this->res['data'] = $idcard;
-            // return false;
-        }
+
+        // $this->res['data'] = strlen($idcard) != 18;
         return $this->res;
     }
     public function phoneNumber($telnum)
